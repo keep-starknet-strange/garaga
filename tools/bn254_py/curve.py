2ef765b26ea6f08f6892cdfa29ea490c12609aab
omnisats <omnisats@protonmail.com>
2023-01-02 15:55:09 +0100

    🚧 add tools

---
+++ tools/bn254_py/curve.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+#
+# Copyright (c) 2012-2020 MIRACL UK Ltd.
+#
+# This file is part of MIRACL Core
+# (see https://github.com/miracl/core).
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# lib curve constants
+
+from bn254_py.constants import *
+
+SHA = 'sha256'   # hash type to use with this curve
+EFS = 32   # Elliptic curve Field Size in bytes
+CurveType = WEIERSTRASS
+CurveCof = 1
+
+SexticTwist = D_TYPE
+SignOfX = 1
+PairingFriendly = BN
+QNRI = 0
+
+x = 0x4080000000000001 # WRONG ! 
+x = 4965661367192848881 # correct for bn254
+# x = 4593689212103950336 # paper fast 
+p = 36*x**4 + 36*x**3 + 24*x**2 + 6 * x + 1
+if SignOfX == NEGATIVEX:
+    p = 36 * x * x * x * x - 36 * x * x * x + 24 * x * x - 6 * x + 1
+else:
+    p = 36 * x * x * x * x + 36 * x * x * x + 24 * x * x + 6 * x + 1
+
+t = 6 * x * x + 1
+r = p + 1 - t
+
+# elliptic curve
+A = 0
+B = 2
+
+# generator point on G1
+Gx = p - 1
+Gy = 1
+
+# Frobenius constant
+Fra = 0x1B377619212E7C8CB6499B50A846953F850974924D3F77C2E17DE6C06F2A6DE9
+Frb = 0x9EBEE691ED1837503EAB22F57B96AC8DC178B6DB2C08850C582193F90D5922A
+
+# Generator point on G2
+Pxa = 0x61A10BB519EB62FEB8D8C7E8C61EDB6A4648BBB4898BF0D91EE4224C803FB2B
+Pxb = 0x516AAF9BA737833310AA78C5982AA5B1F4D746BAE3784B70D8C34C1E7D54CF3
+Pya = 0x21897A06BAF93439A90E096698C822329BD0AE6BDBE09BD19F0E07891CD2B9A
+Pyb = 0xEBB2B0E7C8B15268F6D4456F5F38D37B09006FFD739C9578A2D1AEC6B3ACE9B
---
+++ tools/mcl.py
@@ -0,0 +1,104 @@
+import mclbn256 as mcl
+from mclbn256 import GT, G2, G1
+from bn254 import Fp2, Fp12, Fp4, Fp, ECp2, curve
+
+P=curve.p
+def xy(x):
+    # Convert G2 point from mcl lib to python tuple of 4 coordinates 
+    xcord=str(x.tostr()).replace("'",'').split(' ')[1:]
+    print(xcord)
+    return (int(xcord[0],16), int(xcord [1],16), int(xcord[2],16), int(xcord[3],16))
+
+def xyGT(x):
+    # Convert GT point from mcl lib to python tuple of 12 coordinates. 
+
+    xcord=str(x.tostr().decode()).replace("'",'').split(' ')
+    # print(xcord)
+    return (int(xcord[0],16), int(xcord [1],16), int(xcord[2],16), int(xcord[3],16),
+    int(xcord[4],16), int(xcord[5],16), int(xcord[6],16), int(xcord[7],16),
+    int(xcord[8],16), int(xcord[9],16), int(xcord[10],16), int(xcord[11],16), 
+    )
+
+
+g2:G2=mcl.G2.base_point()
+g1:G1=mcl.G1.base_point()
+
+g22:G2=g2.dbl()
+
+
+e_g1g2:GT=g2.pairing(g1)
+mul_GT:GT=e_g1g2.mul(e_g1g2).mul(e_g1g2)
+pow_GT:GT=e_g1g2.pow(mcl.Fr(3))
+
+ee=xyGT(e_g1g2)
+print('E(g1,g2)=', xyGT(e_g1g2))
+print('E(g1,g2)**2=', xyGT(mul_GT))
+
+
+e_g1g2_py = Fp12(
+    Fp4(Fp2(Fp(ee[0]), Fp(ee[1])), Fp2(Fp(ee[2]), Fp(ee[3]))),
+    Fp4(Fp2(Fp(ee[4]), Fp(ee[5])), Fp2(Fp(ee[6]), Fp(ee[7]))),
+    Fp4(Fp2(Fp(ee[8]), Fp(ee[9])), Fp2(Fp(ee[10]), Fp(ee[11]))))
+mul_fq12 = e_g1g2_py.pow(2)
+
+xyg2=xy(g2)
+r=xy(g22)
+
+x0=Fp2(Fp(r[0]), Fp(r[1]))
+y0=Fp2(Fp(r[2]), Fp(r[3]))
+
+x1=Fp2(Fp(xyg2[0]), Fp(xyg2[1]))
+y1=Fp2(Fp(xyg2[2]), Fp(xyg2[3]))
+
+
+t=y0-y1
+print("T=",t)
+u=x0-x1
+print("U=", u)
+slope = t*u.inverse()
+print("slope=", slope)
+x_diff_slope=u*slope
+print("x_diff_slope=", x_diff_slope)
+
+res=x_diff_slope-y0
+print(res)
+print(y1)
+res=res+y1
+print(res)
+
+slope_sqr=slope*slope
+new_x=slope_sqr - x0 - x1
+new_y=slope*(x0-new_x) - y0
+print("new_x=",new_x)
+print("new_y=",new_y)
+pt0=ECp2()
+pt0.set(x0,y0)
+pt1=ECp2()
+pt1.set(x1,y1)
+
+pt2 = pt0.add(pt1)
+print("PT2=", pt2)
+
+
+
+def from_uint(a):
+    return a[0] + (a[1] << 128)
+
+
+def split_128(a):
+    return (a & ((1 << 128) - 1), a >> 128)
+
+    
+def to_bigint(a):
+
+    RC_BOUND = 2 ** 128
+    BASE = 2**86
+    low, high = split_128(a)
+    D1_HIGH_BOUND = BASE ** 2 // RC_BOUND
+    D1_LOW_BOUND = RC_BOUND // BASE
+    d1_low, d0 = divmod(low, BASE)
+    d2, d1_high = divmod(high, D1_HIGH_BOUND)
+    d1 = d1_high * D1_LOW_BOUND + d1_low
+
+    return (d0, d1, d2)
+
