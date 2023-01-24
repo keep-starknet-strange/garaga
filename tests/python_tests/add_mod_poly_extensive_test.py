2d9e5ef842b885b617d1ed63ddd92d8c9b07ce98
feltroid Prime <feltroidprime@protonmail.com>
2023-01-24 10:34:54 +0100

    ♻️ add mod poly Cython impl. and testing

--- tests/python_tests/add_mod_poly_extensive_test.py
+++ tests/python_tests/add_mod_poly_extensive_test.py
@@ -1,22 +1,49 @@
-import sys
 import time
+from tools.py.add_mod_poly import *
 
-print(sys.path)
-
-from tools.py.add_mod_poly_c import test_full_field
-from tools.py.polynome import evaluate_poly, load_coeff
-
-m, t, s, P = load_coeff(3)
-p=evaluate_poly(P, t)
+# Test Cython functions from add_mod_poly files.
+# Used for research purposes.
 
 
-print(t)
-print(p, p.bit_length())
+# SystemError is used to catch C errors when cdef functions with except -1 in their signatures are called.
 
 
-t0=time.time()
-test_full_field(n_cores=20)
-t1=time.time()
+def log(x, *args):
+    print("\n ============", x, *args)
 
-print(t1-t0)
+try:
+    log("Testing polyadd using evaluated polynomials in x < t:")
+    test_full_field_add_mod_reduced_t(t_case=5)
+except SystemError:
+    log("Test failed (as expected).")
+    
 
+## Test the full field 
+try:
+    log("Test polyadd parallel:")
+    t0=time.time()
+    test_full_field_polyadd(n_cores=20, t_case = 5, subfield=True)
+    t1=time.time()
+    log("Test succeeded in ", time.time()-t0, "sec.")
+except SystemError:
+    t1=time.time()
+    log("Test Failed in ", t1-t0, "sec")
+    log("/!\ Test failed in test add_mod_poly_full_field (unexpected).")
+
+try:
+    log("Test polyadd_3 parallel:")
+    t0=time.time()
+    test_polyadd_3(t_case = 5, n_cores=20)
+    t1=time.time()
+    log("Test succeeded in ", time.time()-t0, "sec.")
+except SystemError:
+    t1=time.time()
+    log("Test Failed in ", t1-t0, "sec")
+    log("/!\ Test failed in test garagadd_3 (unexpected).")
+
+
+log("Test full field polynomial decomposition with euclidean division:")
+test_full_field_decomposition(t_case=5)
+
+log("Test if A(x) > P(x) => A(t) > P(t) for x < t:")
+test_assertion(t_case=5)
