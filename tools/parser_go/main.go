10e9365b6b7608fdb4da00e6fd70272e1e88dc39
Bachir ARIF <arif.bachir@hotmail.com>
2023-01-29 16:19:49 +0100

    FEAT: Refactoring code and add the specific folder from gnark

--- tools/parser_go/main.go
+++ tools/parser_go/main.go
@@ -2,39 +2,13 @@ package main
 
 import (
 	"fmt"
+	"github.com/bacharif/garaga/internal/fptower"
 	"github.com/consensys/gnark-crypto/ecc/bn254/fp"
 	"github.com/urfave/cli"
 	"log"
 	"math/big"
 	"os"
 )
-
-// E2 is a degree two finite field extension of fp.Element
-type E2 struct {
-	A0, A1 fp.Element
-}
-
-// Add adds two elements of E2
-func (z *E2) Add(x, y *E2) *E2 {
-	addE2(z, x, y)
-	return z
-}
-
-func addE2(z, x, y *E2) {
-	z.A0.Add(&x.A0, &y.A0)
-	z.A1.Add(&x.A1, &y.A1)
-}
-
-// Sub two elements of E2
-func (z *E2) Sub(x, y *E2) *E2 {
-	subE2(z, x, y)
-	return z
-}
-
-func subE2(z, x, y *E2) {
-	z.A0.Sub(&x.A0, &y.A0)
-	z.A1.Sub(&x.A1, &y.A1)
-}
 
 var app = cli.NewApp()
 
@@ -48,7 +22,7 @@ func info() {
 func main() {
 	info()
 	app.Action = func(c *cli.Context) error {
-		var z, x, y E2
+		var z, x, y fptower.E2
 		var A0, A1, A2, A3 fp.Element
 		n := new(big.Int)
 		n, _ = n.SetString(c.Args().Get(1), 10)
@@ -71,6 +45,10 @@ func main() {
 			z.Add(&x, &y)
 		case "sub":
 			z.Sub(&x, &y)
+		case "div":
+			z.Div(&x, &y)
+		case "mul":
+			z.Mul(&x, &y)
 		}
 
 		fmt.Println(z)
