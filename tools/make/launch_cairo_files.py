262d1b7cf4879b688c887ea19c6a0e0390acad71
feltroid Prime <feltroidprime@protonmail.com>
2023-04-01 09:33:46 +0200

    🔧 update profile script

--- tools/make/launch_cairo_files.py
+++ tools/make/launch_cairo_files.py
@@ -6,16 +6,25 @@ import readline
 import blake3
 import json
 from tinydb import TinyDB, Query
+import subprocess
 
+CAIRO_PROGRAMS_FOLDERS = ["tests/cairo_programs/", "tests/cairo_snark/groth16/"]
+DEP_FOLDERS = ["src/bn254/", "src"]
 
-PATH_CAIRO_PROGRAMS  = "tests/cairo_programs"
-CAIRO_FILES = [f for f in listdir(PATH_CAIRO_PROGRAMS) if isfile(join(PATH_CAIRO_PROGRAMS, f)) if f[-6:]=='.cairo']
-LIB_CAIRO_FILES = ["src/bn254/"+f for f in listdir("src/bn254") if isfile(join("src/bn254", f)) if f[-6:]=='.cairo']
-LIB_CAIRO_FILES += ["src/bn254/towers/" + f for f in listdir("src/bn254/towers") if isfile(join("src/bn254/towers", f)) if f[-6:]=='.cairo']
-print(LIB_CAIRO_FILES)
-def get_hash_if_file_exists(file_path:str)-> str:
+CAIRO_PROGRAMS = []
+for folder in CAIRO_PROGRAMS_FOLDERS:
+    CAIRO_PROGRAMS+= [join(folder, f) for f in listdir(folder) if isfile(join(folder, f)) if f.endswith('.cairo')]
+print(CAIRO_PROGRAMS)
+
+# Get all dependency files
+DEP_FILES = []
+for dep_folder in DEP_FOLDERS:
+    DEP_FILES += [join(dep_folder, f) for f in listdir(dep_folder) if isfile(join(dep_folder, f)) if f.endswith('.cairo')]
+print(DEP_FILES)
+
+def get_hash_if_file_exists(file_path: str) -> str:
     isExist = os.path.exists(file_path)
-    if isExist==False:
+    if isExist == False:
         return None
     else:
         json_bytes = open(file_path, "rb")
@@ -23,7 +32,7 @@ def get_hash_if_file_exists(file_path:str)-> str:
         hash = blake3.blake3(bytes).digest()
         return str(hash)
 
-def mkdir_if_not_exists(path:str):
+def mkdir_if_not_exists(path: str):
     isExist = os.path.exists(path)
     if not isExist:
         os.makedirs(path)
@@ -31,11 +40,10 @@ def mkdir_if_not_exists(path:str):
 
 mkdir_if_not_exists("build")
 mkdir_if_not_exists("build/compiled_cairo_files")
-
 mkdir_if_not_exists("build/profiling")
 
-def complete(text,state):
-    volcab = CAIRO_FILES
+def complete(text, state):
+    volcab = [x.split('/')[-1] for x in CAIRO_PROGRAMS]
     results = [x for x in volcab if x.startswith(text)] + [None]
     return results[state]
 
@@ -43,35 +51,43 @@ readline.parse_and_bind("tab: complete")
 readline.set_completer(complete)
 
 FILENAME_DOT_CAIRO = input('\n>>> Enter cairo program to run or double press for suggestions <TAB> : \n\n')
+
+# Find the full local path for the selected Cairo file
+for cairo_path in CAIRO_PROGRAMS:
+    if cairo_path.endswith(FILENAME_DOT_CAIRO):
+        FILENAME_DOT_CAIRO_PATH = cairo_path
+        break
+else:
+    raise FileNotFoundError(f"File {FILENAME_DOT_CAIRO} not found in the Cairo programs folders.")
+
+print(f"Selected Cairo file: {FILENAME_DOT_CAIRO_PATH}")
+
 FILENAME = FILENAME_DOT_CAIRO.removesuffix('.cairo')
-FILENAME_DOT_CAIRO_PATH = PATH_CAIRO_PROGRAMS+'/'+FILENAME_DOT_CAIRO
 JSON_INPUT_PATH = FILENAME_DOT_CAIRO_PATH.replace('.cairo', '_input.json')
 
 input_exists = os.path.exists(JSON_INPUT_PATH)
 
 mkdir_if_not_exists(f"build/profiling/{FILENAME}")
 
+# Combine main and dependency files
+ALL_FILES = CAIRO_PROGRAMS + DEP_FILES
+
 def write_all_hash(db:TinyDB):
-    for FILE_DOT_CAIRO in CAIRO_FILES:
-        db.insert({'name':FILE_DOT_CAIRO, 'hash':get_hash_if_file_exists(f"{PATH_CAIRO_PROGRAMS}/{FILE_DOT_CAIRO}")})
-    for FILE_DOT_CAIRO in LIB_CAIRO_FILES:
-        db.insert({'name':FILE_DOT_CAIRO, 'hash':get_hash_if_file_exists(FILE_DOT_CAIRO)})
+    for FILE in ALL_FILES:
+        db.insert({'name':FILE, 'hash':get_hash_if_file_exists(FILE)})
 
 def get_all_hash():
     r = []
-    for FILE_DOT_CAIRO in CAIRO_FILES:
-        r.append(get_hash_if_file_exists(f"{PATH_CAIRO_PROGRAMS}/{FILE_DOT_CAIRO}"))
-    for FILE_DOT_CAIRO in LIB_CAIRO_FILES:
-        r.append(get_hash_if_file_exists(FILE_DOT_CAIRO))
+    for FILE in ALL_FILES:
+        r.append(get_hash_if_file_exists(FILE))
     return r 
 
 
 db = TinyDB(f"build/programs_hash.json")
 
-if len(db)!=(len(CAIRO_FILES)+len(LIB_CAIRO_FILES)):
+if len(db)!=(len(ALL_FILES)):
     db.remove(Query().name!=0)
     write_all_hash(db)
-
 
 hash_table = db.all()
 current_hash_table = get_all_hash()
@@ -89,40 +105,33 @@ prev_hash = get_hash_if_file_exists(f"build/compiled_cairo_files/{FILENAME}.json
 if did_some_file_changed() or os.path.exists(f"build/compiled_cairo_files/{FILENAME}.json")==False:
     print(f"Compiling {FILENAME_DOT_CAIRO} because some files have changed ... ")
 
-    os.system(f"cairo-compile {PATH_CAIRO_PROGRAMS}/{FILENAME_DOT_CAIRO} --output build/compiled_cairo_files/{FILENAME}.json")
+    os.system(f"cairo-compile {FILENAME_DOT_CAIRO_PATH} --output build/compiled_cairo_files/{FILENAME}.json")
 else:
     print(f"Skipping compilation for {FILENAME_DOT_CAIRO} since no file has changed.")
 
 
 new_hash = get_hash_if_file_exists(f"build/compiled_cairo_files/{FILENAME}.json")
+if input_exists:
+    print(f"Running {FILENAME_DOT_CAIRO} with input {JSON_INPUT_PATH} ... ")
+    
+    cmd=f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --program_input={JSON_INPUT_PATH} --layout=starknet_with_keccak --print_output --profile_output ./build/profiling/{FILENAME}/profile.pb.gz --cairo_pie_output ./build/profiling/{FILENAME}/{FILENAME}_pie.zip"
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True,text=True)
 
-
-if new_hash!=prev_hash:
-
-    if input_exists:
-        print(f"Running {FILENAME_DOT_CAIRO} with input {JSON_INPUT_PATH} ... ")
-
-        os.system(f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --program_input={JSON_INPUT_PATH} --layout=all --print_output --profile_output ./build/profiling/{FILENAME}/profile.pb.gz ")
-    else:
-        print(f"Running {FILENAME_DOT_CAIRO} ... ")
-
-        os.system(f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --layout=all --print_output --profile_output ./build/profiling/{FILENAME}/profile.pb.gz ")
-    print(f"Running profiling tool for {FILENAME_DOT_CAIRO} because the compiled file has changed ... ")
-
-    os.system(f"cd ./build/profiling/{FILENAME} && go tool pprof -png profile.pb.gz ")
 else:
-    print(f"Running {FILENAME_DOT_CAIRO} without profiling ...")
-    if input_exists:
-        print(f"Running {FILENAME_DOT_CAIRO} with input {JSON_INPUT_PATH} ... ")
+    print(f"Running {FILENAME_DOT_CAIRO} ... ")
+    cmd=f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --layout=starknet_with_keccak --print_output --profile_output ./build/profiling/{FILENAME}/profile.pb.gz --cairo_pie_output ./build/profiling/{FILENAME}/{FILENAME}_pie.zip"
+    process = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.STDOUT,shell=True,text=True)
 
-        os.system(f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --program_input={JSON_INPUT_PATH} --layout=all --print_output ")
-    else:
-        print(f"Running {FILENAME_DOT_CAIRO} ... ")
 
-        os.system(f"cairo-run --program=build/compiled_cairo_files/{FILENAME}.json --layout=all --print_output ")
+print(f"Running profiling tool for {FILENAME_DOT_CAIRO} because the compiled file has changed ... ")
 
-    print(f"Profiling for {FILENAME_DOT_CAIRO} should already be available in /build/profiling/{FILENAME} ! ")
+os.system(f"cd ./build/profiling/{FILENAME} && go tool pprof -png profile.pb.gz ")
+stdout_file = f"build/profiling/{FILENAME}/{FILENAME}_stdout.txt"
 
+with open(stdout_file, "w") as f:
+    for line in process.stdout:
+        print(line, end="")
+        f.write(line)
 
 def format_stdout(file_path:str)-> str:
     isExist = os.path.exists(file_path)
@@ -131,17 +140,12 @@ def format_stdout(file_path:str)-> str:
     else:
         stdout = open(file_path, "r")
         stdout = stdout.read()
-        # print(stdout)
         program_output=stdout.split('Program output:\n')[1].replace(' ','').split('\n')
         program_output=[int(x) for x in list(filter(None,program_output))]
         program_output_dict = dict(zip(range(len(program_output)), program_output))
-        print(program_output)
-        with open(f"tests/profiling/{FILENAME}/{FILENAME}_output.json", 'w') as fp:
+        print(f"Program output: \n{program_output}")
+        with open(f"build/profiling/{FILENAME}/{FILENAME}_output.json", 'w') as fp:
             json.dump(program_output_dict, fp)
 
 
-
-format_stdout(f"tests/profiling/{FILENAME}/{FILENAME}_stdout.txt")
-
-
-
+format_stdout(f"build/profiling/{FILENAME}/{FILENAME}_stdout.txt")
