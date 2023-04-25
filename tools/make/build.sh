01010ea6b789cd40989c470f6042c43169f3f5e4
feltroid Prime <feltroidprime@protonmail.com>
2023-04-25 22:08:27 +0200

    compiles starknet contracts or cairo programs accordingly

--- tools/make/build.sh
+++ tools/make/build.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
+
+process_cairo_file() {
+    local cairo_file="$1"
+    local filename=$(basename "$cairo_file" .cairo)
+    local first_line=$(head -n 1 "$cairo_file")
+
+    if [[ "$first_line" == "%lang starknet" ]]; then
+        echo "Compiling $cairo_file using starknet-compile ..."
+        starknet-compile "$cairo_file" --output "build/compiled_cairo_files/$filename.json" --abi "build/compiled_cairo_files/$filename_abi.json"
+    else
+        echo "Compiling $cairo_file using cairo-compile ..."
+        cairo-compile "$cairo_file" --output "build/compiled_cairo_files/$filename.json"
+    fi
+}
+
 cairo_files=$(find ./src -name "*.cairo")
 
 for cairo_file in $cairo_files
 do
-    filename=$(basename $cairo_file .cairo)
-    echo "Compiling $cairo_file ..."
-    cairo-compile $cairo_file --output "build/compiled_cairo_files/$filename.json"
+    process_cairo_file "$cairo_file"
 done
 
 cairo_files=$(find ./tests/cairo_programs -name "*.cairo")
 
 for cairo_file in $cairo_files
 do
-    filename=$(basename $cairo_file .cairo)
-    echo "Compiling $cairo_file ..."
-    cairo-compile $cairo_file --output "build/compiled_cairo_files/$filename.json"
+    process_cairo_file "$cairo_file"
 done
-
