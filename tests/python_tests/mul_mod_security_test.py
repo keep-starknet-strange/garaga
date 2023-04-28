a70e7e7423eb27ea4051101d4cdb46fa24536cc7
feltroid Prime <feltroidprime@protonmail.com>
2023-04-28 13:02:21 +0200

    fix mul_ab, mul_ab_sub_c polynomial operations

--- tests/python_tests/mul_mod_security_test.py
+++ tests/python_tests/mul_mod_security_test.py
@@ -1,5 +1,6 @@
 import random
 import sympy
+from tools.py.garaga_mul_mod import test_hack_mul, test_mul_ab, test_mul_ab_sub_c, prime, prime2
 
 def generate_prime(n):
     while True:
@@ -16,3 +17,10 @@ def generate_prime(n):
 p_stark=generate_prime(5)
 p_emul=generate_prime(6)
 
+test_mul_ab()
+test_mul_ab_sub_c()
+
+try:
+    test_hack_mul(4)
+except Exception as e:
+    print(f"Error: {e}")
