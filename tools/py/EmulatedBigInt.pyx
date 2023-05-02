a5f0f278773cb9dc5cd35c781e5b40a184930fbb
feltroid Prime <feltroidprime@protonmail.com>
2023-05-02 13:40:09 +0200

    🚧 try: Hack Mul impl.

--- tools/py/EmulatedBigInt.pyx
+++ tools/py/EmulatedBigInt.pyx
@@ -1,22 +1,33 @@
 from libc.stdint cimport int64_t, uint8_t
-from libc.stdlib cimport malloc, free
+from libc.stdlib cimport malloc, realloc, free
 cimport cython 
+from cython.parallel import prange, parallel
+from openmp cimport omp_get_thread_num, omp_lock_t, omp_init_lock, omp_destroy_lock, omp_set_lock, omp_unset_lock
+import numpy as np
+cimport numpy as np
 ctypedef int64_t INT64
+from libc.stdlib cimport malloc, realloc, free
+from cpython.list cimport PyList_New, PyList_Append
 
 ctypedef INT64* BigIntPtr
 ctypedef INT64* Int64Ptr
 
-# cdef struct BigIntPair:
-#     BigIntPtr x
-#     BigIntPtr y
+cdef struct BigIntPair:
+    BigIntPtr x
+    BigIntPtr y
 
-# ctypedef BigIntPair* BigIntPairPtr
+cdef struct ResultTuple:
+    int x
+    int y
+ctypedef ResultTuple* ResultTuplePtr
+ctypedef BigIntPair* BigIntPairPtr
 
 
 cdef class EmulatedBigInt:
     cdef:
         uint8_t n_limbs
         uint8_t unreduced_n_limbs
+        uint8_t n_cores
         INT64 base
         INT64 base_inverse
         INT64 native_prime
@@ -24,8 +35,9 @@ cdef class EmulatedBigInt:
         BigIntPtr limbs
         BigIntPtr emulated_prime_limbs
 
-    def __cinit__(self, uint8_t n_limbs, INT64 base, INT64 native_prime, INT64 emulated_prime, INT64 value=0):
+    def __cinit__(self, uint8_t n_limbs, uint8_t n_cores, INT64 base, INT64 native_prime, INT64 emulated_prime, INT64 value=0):
         self.n_limbs = n_limbs
+        self.n_cores = n_cores
         self.unreduced_n_limbs = (n_limbs-1)*2 + 1
         self.base = base
         self.base_inverse = modular_inverse(base, native_prime)
@@ -86,10 +98,55 @@ cdef class EmulatedBigInt:
             return 1
         else:
             return 0
-    cdef int hack_mul(self, EmulatedBigInt other):
+    cdef hack_mul(self, EmulatedBigInt other):
+        cdef int i, j, result
+        cdef INT64 cardinal_f_pow_n_limbs = 1
+        for i in range(self.n_limbs):
+            cardinal_f_pow_n_limbs *= self.native_prime
 
-        return 0
+        cdef BigIntPtr malicious_q, malicious_r
+        cdef uint8_t n_cores = self.n_cores
+        cdef ResultTuple **results_list
+        cdef int *results_count
+        cdef omp_lock_t results_lock
+        cdef int thread_id
 
+        # Initialize storage for results_list and results_count
+        results_list = <ResultTuple **> malloc(n_cores * sizeof(ResultTuplePtr))
+        results_count = <int *> malloc(n_cores * sizeof(int))
+        for i in range(n_cores):
+            results_count[i] = 0
+            results_list[i] = NULL
+        omp_init_lock(&results_lock)
+
+        with nogil:
+            with parallel(num_threads=n_cores):
+                thread_id = omp_get_thread_num()
+                for i in prange(cardinal_f_pow_n_limbs, schedule='static'):
+                    for j in range(cardinal_f_pow_n_limbs):
+                        malicious_q = split_int64(i, self.native_prime, self.n_limbs)
+                        malicious_r = split_int64(j, self.native_prime, self.n_limbs)
+                        result = self.mul_malicious(other, malicious_q, malicious_r)
+                        if result == 1:
+                            # Lock the results_list and results_count to avoid race conditions
+                            omp_set_lock(&results_lock)
+                            results_list[thread_id] = <ResultTuple *> realloc(results_list[thread_id], (results_count[thread_id] + 1) * sizeof(ResultTuple))
+                            results_list[thread_id][results_count[thread_id]].x = i
+                            results_list[thread_id][results_count[thread_id]].y = j
+                            results_count[thread_id] += 1
+                            omp_unset_lock(&results_lock)
+
+        # Process the results
+        cdef list py_results = PyList_New(0)
+        for i in range(n_cores):
+            for j in range(results_count[i]):
+                PyList_Append(py_results, (results_list[i][j].x, results_list[i][j].y))
+
+        # Free memory
+        free(results_list)
+        free(results_count)
+        omp_destroy_lock(&results_lock)
+        return py_results
 
 
     def __str__(self):
