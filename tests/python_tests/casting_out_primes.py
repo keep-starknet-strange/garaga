5e3956eae57ae941f30803be3ed76e96e3498221
feltroid Prime <feltroidprime@protonmail.com>
2023-05-06 14:18:00 +0200

    M coprime set & witness

--- tests/python_tests/casting_out_primes.py
+++ tests/python_tests/casting_out_primes.py
@@ -1,6 +1,7 @@
 import random
 from typing import Tuple, List
 from math import lcm
+from sympy import gcd
 
 p = 3618502788666131213697322783095070105623107215331596699973092056135872020481
 q = 21888242871839275222246405745257275088696311157297823662689037894645226208583
@@ -9,7 +10,7 @@ n = 3 # number of limbs
 n_pi = 2*n - 1 # number of limbs in polynomial product
 b = 2**86 # base
 
-def get_felt(p:int):
+def get_felt(p:int) -> int:
     return random.randint(0, p-1)
 
 def split_fq(x:int) -> List[int]:
@@ -23,7 +24,7 @@ def split_fq(x:int) -> List[int]:
     return coeffs[::-1]
 
 # evaluate x(b)
-def sigma_b(x:list):
+def sigma_b(x:list) -> int:
     result = 0
     for i in range(len(x)):
         assert x[i] < b, f"Error: wrong bounds {x[i]} >= {b}"
@@ -44,7 +45,7 @@ def pi_b(x:list, y:list) -> Tuple[List[int], int]:
     return limbs, result
 
 # evaluate x(b) mod m
-def sigma_b_mod_m(x:list, m:int):
+def sigma_b_mod_m(x:list, m:int) -> int:
     result = 0
     assert len(x) == n_pi or len(x) == n, "Error: sigma_b_mod_m() requires a list of length n or n_pi"
     for i in range(len(x)):
@@ -65,6 +66,40 @@ def pi_b_mod_m(x:list, y:list, m:int) -> Tuple[List[int], int]:
     assert 0 <= result < n**2*m*b**2
     return limbs, result
 
+def get_m_bound() -> int:
+    return p//(4*(n**2)*(b**2))
+def get_lcm_bound() -> int:
+    return 2*(n**2)*q*(b**2)
+
+def generate_coprime_set(m_bound, lcm_bound) -> List[int]:
+    M=[]
+    # Check if a candidate number is coprime to all elements in M
+    def is_coprime_to_all(candidate, M):
+        return all(gcd(candidate, m) == 1 for m in M)
+    candidate = m_bound
+    while candidate >= 2:
+        if is_coprime_to_all(candidate, M):
+            M.append(candidate)
+        if lcm(*M) >= lcm_bound:
+            break
+        candidate -= 1
+    return M
+
+def get_witness_z_and_r(x:list, y:list):
+    assert len(x) == len(y) == n, "Error: get_witness_z_and_q() requires two lists of length n"
+    z:list = split_fq(sigma_b(x) * sigma_b(y) % q)
+    pi:int = pi_b_mod_m(x, y, q)[1]
+    sigma:int = sigma_b_mod_m(z, q)
+    r_q = pi - sigma
+    assert r_q % q == 0, "Error: r_q is not divisible by q"
+    r = r_q // q
+    return z, r
+
+
+m_bound = get_m_bound()
+lcm_bound = get_lcm_bound()
+M = generate_coprime_set(m_bound, lcm_bound)
+print("Coprime set M:", set(M))
 
 x_o,y_o = get_felt(q), get_felt(q)
 x,y = split_fq(x_o), split_fq(y_o)
@@ -74,4 +109,4 @@ assert sigma_b(x) == x_o, "Error: sigma_b() is not working"
 assert sigma_b(y) == y_o, "Error: sigma_b() is not working"
 assert pi_b(max, max)[1] == (q-1)**2, "Error: pi_b() is not working"
 assert sigma_b_mod_m(max, q) == q-1, "Error: sigma_b_mod_m() is not working"
-
+v = get_witness_z_and_r(x, y)
