634285f70ba7f332c66602a9816a8575b41d0c37
feltroid Prime <feltroidprime@protonmail.com>
2023-10-16 11:31:47 +0200

    fast poseidon patch

--- tools/make/setup.sh
+++ tools/make/setup.sh
@@ -6,6 +6,8 @@ python3.9 -m venv venv
 echo 'export PYTHONPATH="$PWD:$PYTHONPATH"' >> venv/bin/activate
 source venv/bin/activate
 pip install -r tools/make/requirements.txt
+echo "Patching poseidon_utils.py"
+patch venv/lib/python3.9/site-packages/starkware/cairo/common/poseidon_utils.py tools/make/poseidon_utils.patch
 pip install gmpy2 --target ~/.protostar/dist/protostar
 
 if [ "$system" = "Darwin" ]; then
