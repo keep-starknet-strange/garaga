20959ebc1a4beb80886a8b188f97a8e0c2823ef1
feltroid Prime <feltroidprime@protonmail.com>
2023-10-30 17:24:05 +0100

    1:1 Pre-hash computation

--- tools/py/pairing_curves/bn254/final_exp.py
+++ tools/py/pairing_curves/bn254/final_exp.py
@@ -2,6 +2,7 @@ from tools.py.polynomial import Polynomial
 from tools.py.field import BaseFieldElement, BaseField
 from tools.py.extension_trick import (
     gnark_to_v,
+    gnark_to_v_bigint3,
     flatten,
     neg_e6,
     v_to_gnark,
@@ -25,6 +26,7 @@ def split_128(a):
 
 
 p = 0x30644E72E131A029B85045B68181585D97816A916871CA8D3C208C16D87CFD47
+STARK = 3618502788666131213697322783095070105623107215331596699973092056135872020481
 field = BaseField(p)
 
 
@@ -49,34 +51,67 @@ def to_fp6(x: list) -> Polynomial:
     return Polynomial([BaseFieldElement(xi, field) for xi in x])
 
 
-def mul_torus(y1: list, y2: list, continuable_hash: int):
-    num_min_v, continuable_hash = mul_trick_e6(y1, y2, continuable_hash)
-    print(f"hash_multorus= {continuable_hash}")
+def mul_torus(
+    y1: list, y2: list, continuable_hash: int, y1_bigint3=None, y2_bigint3=None
+):
+    num_min_v, continuable_hash = mul_trick_e6(
+        y1, y2, continuable_hash, x_bigint3=y1_bigint3, y_bigint3=y2_bigint3
+    )
     num_min_v[1] = num_min_v[1] + 1
+    v1_bigint3 = split(num_min_v[1] - 1)
+    v1_bigint3 = (v1_bigint3[0] + 1, v1_bigint3[1], v1_bigint3[2])
+
     num = num_min_v
     den = [y1i + y2i for y1i, y2i in zip(y1, y2)]
-    res, continuable_hash = div_trick_e6(num, den, continuable_hash)
+    if y1_bigint3 is None:
+        y1_bigint3 = [split(x) for x in y1]
+    if y2_bigint3 is None:
+        y2_bigint3 = [split(x) for x in y2]
+
+    den_bigint3 = [
+        (y1i[0] + y2i[0], y1i[1] + y2i[1], y1i[2] + y2i[2])
+        for y1i, y2i in zip(y1_bigint3, y2_bigint3)
+    ]
+    res, continuable_hash = div_trick_e6(
+        num, den, continuable_hash, y_bigint3=den_bigint3
+    )
     return res, continuable_hash
 
 
-def div_trick_e6(x: list, y: list, continuable_hash: int) -> (list, int):
+def div_trick_e6(
+    x: list, y: list, continuable_hash: int, x_bigint3=None, y_bigint3=None
+) -> (list, int):
     x_gnark, y_gnark = pack_e6(v_to_gnark(x)), pack_e6(v_to_gnark(y))
     div = flatten(div_e6(x_gnark, y_gnark))
     div = gnark_to_v(div)
-    check, h = mul_trick_e6(y, div, continuable_hash)
+    check, h = mul_trick_e6(
+        y, div, continuable_hash, x_bigint3=y_bigint3, y_bigint3=None
+    )
     assert x == check, f"{x} != {check}"
     return div, h
 
 
-def mul_trick_e6(x: list, y: list, continuable_hash: int) -> (list, int):
+def mul_trick_e6(
+    x: list, y: list, continuable_hash: int, x_bigint3=None, y_bigint3=None
+) -> (list, int):
     x_poly, y_poly = to_fp6(x), to_fp6(y)
     z_poly = x_poly * y_poly
     z_polyq = z_poly // unreducible_poly
     z_polyr = z_poly % unreducible_poly
     z_polyq_coeffs = z_polyq.get_coeffs()
     z_polyr_coeffs = z_polyr.get_coeffs()
-    x3 = [split(e) for e in x]
-    y3 = [split(e) for e in y]
+    z_polyq_coeffs = z_polyq_coeffs + [0] * (5 - len(z_polyq_coeffs))
+    z_polyr_coeffs = z_polyr_coeffs + [0] * (6 - len(z_polyr_coeffs))
+    if x_bigint3 is None:
+        x3 = [split(e) for e in x]
+    else:
+        x3 = x_bigint3
+    if y_bigint3 is None:
+        y3 = [split(e) for e in y]
+    else:
+        y3 = y_bigint3
+    # print("multrick x", x3)
+    # print("multrick y", y3)
     q2 = [split_128(e) for e in z_polyq_coeffs]
     r3 = [split(e) for e in z_polyr_coeffs]
     h = poseidon_hash(x3[0][0] * x3[0][1], continuable_hash)
@@ -98,9 +133,13 @@ def mul_trick_e6(x: list, y: list, continuable_hash: int) -> (list, int):
     h = poseidon_hash(y3[4][0] * y3[4][1], h)
     h = poseidon_hash(y3[4][2] * y3[5][0], h)
     h = poseidon_hash(y3[5][1] * y3[5][2], h)
+    # print(f"ch={h}")
+    # print(f"lq2={len(q2)}")
     for i in range(len(q2)):
         h = poseidon_hash(q2[i][0], h)
         h = poseidon_hash(q2[i][1], h)
+    # print(f"ch={h}")
+
     h = poseidon_hash(r3[0][0] * r3[0][1], h)
     h = poseidon_hash(r3[0][2] * r3[1][0], h)
     h = poseidon_hash(r3[1][1] * r3[1][2], h)
@@ -110,12 +149,15 @@ def mul_trick_e6(x: list, y: list, continuable_hash: int) -> (list, int):
     h = poseidon_hash(r3[4][0] * r3[4][1], h)
     h = poseidon_hash(r3[4][2] * r3[5][0], h)
     h = poseidon_hash(r3[5][1] * r3[5][2], h)
+    # print(f"ch={h}")
 
     return z_polyr_coeffs, h
 
 
 def expt_torus(x: list, continuable_hash: int) -> (list, int):
     t3, continuable_hash = square_torus(x, continuable_hash)
+    # print(f"hashsquaretorus={continuable_hash}")
+
     t5, continuable_hash = square_torus(t3, continuable_hash)
     result, continuable_hash = square_torus(t5, continuable_hash)
     t0, continuable_hash = square_torus(result, continuable_hash)
@@ -158,19 +200,34 @@ def n_square_torus(x: list, n: int, continuable_hash: int):
 
 def square_torus(x: list, continuable_hash: int):
     x_gnark = v_to_gnark(x)
-    sq = gnark_to_v(flatten(square_torus_e6(x_gnark)))
-    v_tmp = [2 * sq_i - x_i for sq_i, x_i in zip(sq, x)]
+    sq = [int(x) for x in gnark_to_v(flatten(square_torus_e6(x_gnark)))]
+    v_tmp = [(2 * sq_i - x_i) % p for sq_i, x_i in zip(sq, x)]
+    x_bigint3 = [split(e) for e in x]
+    sq_bigint3 = [split(e) for e in sq]
+    v_tmp_bigint3 = [
+        [
+            (2 * sq_i[0] - x_i[0]) % STARK,
+            (2 * sq_i[1] - x_i[1]) % STARK,
+            (2 * sq_i[2] - x_i[2]) % STARK,
+        ]
+        for sq_i, x_i in zip(sq_bigint3, x_bigint3)
+    ]
     x_poly = to_fp6(v_tmp)
-    y_poly = to_fp6(sq)
+    y_poly = to_fp6(x)
     z_poly = x_poly * y_poly
+
     z_polyq = z_poly // unreducible_poly
     z_polyr = z_poly % unreducible_poly
     z_polyq_coeffs = z_polyq.get_coeffs()
+    # print(f"z_polyq_coeffs={z_polyq_coeffs}")
     z_polyr_coeffs = z_polyr.get_coeffs()
-    x3 = [split(e) for e in v_tmp]
-    y3 = [split(e) for e in sq]
+    # print(f"z_polyr_coeffs={z_polyr_coeffs}")
+    x3 = v_tmp_bigint3
+    y3 = x_bigint3
     q2 = [split_128(e) for e in z_polyq_coeffs]
-
+    # print(f"square_torus_x={x3}")
+    # print(f"square_torus_y={y3}")
+    # print(f"square_torus_q2={q2}")
     h = poseidon_hash(x3[0][0] * x3[0][1], continuable_hash)
     h = poseidon_hash(x3[0][2] * x3[1][0], h)
     h = poseidon_hash(x3[1][1] * x3[1][2], h)
@@ -180,6 +237,7 @@ def square_torus(x: list, continuable_hash: int):
     h = poseidon_hash(x3[4][0] * x3[4][1], h)
     h = poseidon_hash(x3[4][2] * x3[5][0], h)
     h = poseidon_hash(x3[5][1] * x3[5][2], h)
+    # print(f"ch={h}")
     h = poseidon_hash(y3[0][0] * y3[0][1], h)
     h = poseidon_hash(y3[0][2] * y3[1][0], h)
     h = poseidon_hash(y3[1][1] * y3[1][2], h)
@@ -189,9 +247,12 @@ def square_torus(x: list, continuable_hash: int):
     h = poseidon_hash(y3[4][0] * y3[4][1], h)
     h = poseidon_hash(y3[4][2] * y3[5][0], h)
     h = poseidon_hash(y3[5][1] * y3[5][2], h)
+    # print(f"ch={h}")
+    # print(len(q2))
     for i in range(len(q2)):
         h = poseidon_hash(q2[i][0], h)
         h = poseidon_hash(q2[i][1], h)
+    # print(f"ch={h}")
     h = poseidon_hash(1, h)
 
     return sq, h
@@ -240,9 +301,11 @@ def frobenius_torus(x: list):
         18566938241244942414004596690298913868373833782006617400804628704885040364344,
         5722266937896532885780051958958348231143373700109372999374820235121374419868,
     )  # 1 / v^((p-1)/2)
-    res = mul_e6((t0, t1, t2), (v0, (0, 0), (0, 0)))
-    res = gnark_to_v(flatten(res))
-    return res
+    res = flatten(mul_e6((t0, t1, t2), (v0, (0, 0), (0, 0))))
+    res_bigint3 = gnark_to_v_bigint3([split(x) for x in res])
+
+    res = gnark_to_v(res)
+    return res, res_bigint3
 
 
 def frobenius_cube_torus(x: list):
@@ -268,9 +331,10 @@ def frobenius_cube_torus(x: list):
         10190819375481120917420622822672549775783927716138318623895010788866272024264,
         303847389135065887422783454877609941456349188919719272345083954437860409601,
     )  # 1 / v^((p^3-1)/2)
-    res = mul_e6((t0, t1, t2), (v0, (0, 0), (0, 0)))
-    res = gnark_to_v(flatten(res))
-    return res
+    res = flatten(mul_e6((t0, t1, t2), (v0, (0, 0), (0, 0))))
+    res_bigint3 = gnark_to_v_bigint3([split(x) for x in res])
+    res = gnark_to_v(res)
+    return res, res_bigint3
 
 
 def inverse_torus(x: list):
@@ -304,36 +368,49 @@ def final_exponentiation(
     z_c1_full = gnark_to_v(flatten(z_c1))
 
     c, continuable_hash = div_trick_e6(c_num_full, z_c1_full, continuable_hash)
-    print(f"continuable_hash: {continuable_hash}")
+    # print(f"hash={continuable_hash}")
     t0 = frobenius_square_torus(c)
     c, continuable_hash = mul_torus(t0, c, continuable_hash)
-    print(f"continuable_hash: {continuable_hash}")
+    # print(f"hash={continuable_hash}")
 
     t0, continuable_hash = expt_torus(c, continuable_hash)
+    # print(f"hashexpt={continuable_hash}")
     t0 = inverse_torus(t0)
     t0, continuable_hash = square_torus(t0, continuable_hash)
     t1, continuable_hash = square_torus(t0, continuable_hash)
+    # print(f"hash1={continuable_hash}")
     t1, continuable_hash = mul_torus(t0, t1, continuable_hash)
+    # print(f"hash2={continuable_hash}")
     t2, continuable_hash = expt_torus(t1, continuable_hash)
+    # print(f"hash3={continuable_hash}")
     t2 = inverse_torus(t2)
     t3 = inverse_torus(t1)
     t1, continuable_hash = mul_torus(t2, t3, continuable_hash)
+    # print(f"hash4={continuable_hash}")
     t3, continuable_hash = square_torus(t2, continuable_hash)
+    # print(f"hash5={continuable_hash}")
     t4, continuable_hash = expt_torus(t3, continuable_hash)
+    # print(f"hash6={continuable_hash}")
     t4, continuable_hash = mul_torus(t1, t4, continuable_hash)
     t3, continuable_hash = mul_torus(t0, t4, continuable_hash)
     t0, continuable_hash = mul_torus(t2, t4, continuable_hash)
     t0, continuable_hash = mul_torus(c, t0, continuable_hash)
-    t2 = frobenius_torus(t3)
-    t0, continuable_hash = mul_torus(t2, t0, continuable_hash)
+    # print(f"hash7={continuable_hash}")
+    t2, t2_bigint3 = frobenius_torus(t3)
+    t0, continuable_hash = mul_torus(t2, t0, continuable_hash, y1_bigint3=t2_bigint3)
+    # print(f"hash8={continuable_hash}")
     t2 = frobenius_square_torus(t4)
     t0, continuable_hash = mul_torus(t2, t0, continuable_hash)
+    # print(f"hash9={continuable_hash}")
     t2 = inverse_torus(c)
     t2, continuable_hash = mul_torus(t2, t3, continuable_hash)
-    t2 = frobenius_cube_torus(t2)
-
+    # print(f"hash10={continuable_hash}")
+    t2, t2_bigint3 = frobenius_cube_torus(t2)
+    # print(f"hashf={continuable_hash}")
     if unsafe:
-        rest, continuable_hash = mul_torus(t2, t0, continuable_hash)
+        rest, continuable_hash = mul_torus(
+            t2, t0, continuable_hash, y1_bigint3=t2_bigint3
+        )
         res = pack_e6(v_to_gnark(rest))
         res = decompress_torus(res)
         return res, continuable_hash
@@ -356,7 +433,9 @@ def final_exponentiation(
 
         if selector1 == 0:
             if is_zero == 0:
-                rest, continuable_hash = mul_torus(t2, t0t, continuable_hash)
+                rest, continuable_hash = mul_torus(
+                    t2, t0t, continuable_hash, y1_bigint3=t2_bigint3
+                )
                 res = v_to_gnark(rest.get_coeffs())
                 res = decompress_torus(res)
                 return res, continuable_hash
