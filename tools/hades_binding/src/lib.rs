7d515d46a1a9b7851ba311e5ad7f5216be177217
starkfishinator <touficpermis@gmail.com>
2024-03-13 21:02:55 +0700

    have 3 inputs/outputs instead of 2

--- tools/hades_binding/src/lib.rs
+++ tools/hades_binding/src/lib.rs
@@ -17,29 +17,30 @@ fn hades_binding(_py: Python, m: &PyModule) -> PyResult<()> {
 }
 
 #[pyfunction]
-fn hades_permutation(py: Python, py_value_1: &PyBytes, py_value_2: &PyBytes) -> PyResult<PyObject> {
+fn hades_permutation(
+    py: Python,
+    py_value_1: &PyBytes,
+    py_value_2: &PyBytes,
+    py_value_3: &PyBytes,
+) -> PyResult<PyObject> {
     let byte_slice_1: &[u8] = py_value_1.as_bytes();
     let byte_slice_2: &[u8] = py_value_2.as_bytes();
-
-    let number: u32 = 2;
-    let two_bytes = number.to_be_bytes(); // This will give you the big-endian byte representation of 2 as a u32.
-    let mut padded_two_bytes = [0u8; 32];
-    padded_two_bytes[32 - 4..].copy_from_slice(&two_bytes);
+    let byte_slice_3: &[u8] = py_value_3.as_bytes();
 
     let mut state: Vec<FieldElement<Stark252PrimeField>> = vec![
         FieldElement::<Stark252PrimeField>::from_bytes_be(byte_slice_1)
             .expect("Unable to convert first param from bytes to FieldElement"),
         FieldElement::<Stark252PrimeField>::from_bytes_be(byte_slice_2)
             .expect("Unable to convert second param from bytes to FieldElement"),
-        FieldElement::<Stark252PrimeField>::from_bytes_be(&padded_two_bytes)
-            .expect("Unable to convert `two` from bytes to FieldElement"),
+        FieldElement::<Stark252PrimeField>::from_bytes_be(byte_slice_3)
+            .expect("Unable to convert third param from bytes to FieldElement"),
     ];
 
     PoseidonCairoStark252::hades_permutation(&mut state);
 
     let py_result_list = PyList::new(
         py,
-        state[0..2].iter().map(|fe| {
+        state.iter().map(|fe| {
             let fe_bytes = fe.to_bytes_be();
             PyBytes::new(py, &fe_bytes)
         }),
