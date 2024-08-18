aa1ca00f7b6cc77fb41fce77fbc75176d6f3a83a
feltroidprime <feltroidprime@protonmail.com>
2024-08-18 19:58:45 +0200

    test

--- tests/hydra/hints/test_ecip.py
+++ tests/hydra/hints/test_ecip.py
@@ -10,7 +10,7 @@ curves = list(CurveID)
 
 
 @pytest.mark.parametrize("curve_id", curves)
-@pytest.mark.parametrize("msm_size", range(1, 10))
+@pytest.mark.parametrize("msm_size", range(1, 5))
 def test_verify_ecip(curve_id, msm_size):
     curve = CURVES[curve_id.value]
     order = curve.n
@@ -19,19 +19,20 @@ def test_verify_ecip(curve_id, msm_size):
     Bs_G1 = [G1Point.get_nG(curve_id, 1) for _ in range(msm_size)]
     scalars = [random.randint(1, order - 1) for _ in range(msm_size)]
 
-    # Q, sum_dlog = zk_ecip_hint(Bs_G1, scalars, use_rust=False)
+    Q, sum_dlog = zk_ecip_hint(Bs_G1, scalars, use_rust=False)
+
     Q_rust, sum_dlog_rust = zk_ecip_hint(Bs_G1, scalars, use_rust=True)
 
-    # assert Q == Q_rust, f"Q: {Q}, \nQ_rust: {Q_rust}"
-    # assert (
-    #     sum_dlog == sum_dlog_rust
-    # ), f"sum_dlog: {sum_dlog}, \nsum_dlog_rust: {sum_dlog_rust}"
+    assert Q == Q_rust, f"Q: {Q}, \nQ_rust: {Q_rust}"
+    assert (
+        sum_dlog == sum_dlog_rust
+    ), f"sum_dlog: {sum_dlog}, \nsum_dlog_rust: {sum_dlog_rust}"
 
     assert verify_ecip(Bs_G1, scalars, Q=Q_rust, sum_dlog=sum_dlog_rust)
-    # # Test for G2 points if the curve supports pairing
-    # if isinstance(CURVES[curve_id.value], PairingCurve):
-    #     Bs_G2 = [G2Point.gen_random_point(curve_id) for _ in range(msm_size)]
-    #     assert verify_ecip(Bs_G2, scalars)
+    # Test for G2 points if the curve supports pairing
+    if isinstance(CURVES[curve_id.value], PairingCurve):
+        Bs_G2 = [G2Point.gen_random_point(curve_id) for _ in range(msm_size)]
+        assert verify_ecip(Bs_G2, scalars)
 
 
 @pytest.mark.parametrize("curve_id", curves)
