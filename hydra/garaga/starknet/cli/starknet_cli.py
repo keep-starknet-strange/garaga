1f98ef9bfceee85b6f232e908fd81f18e3203d53
feltroidprime <feltroidprime@protonmail.com>
2024-08-24 22:08:49 +0200

    rm hi command

--- hydra/garaga/starknet/cli/starknet_cli.py
+++ hydra/garaga/starknet/cli/starknet_cli.py
@@ -1,6 +1,4 @@
 import typer
-from rich import print
-from typing_extensions import Annotated
 
 from garaga.starknet.cli.declare import declare_project
 from garaga.starknet.cli.deploy import deploy_project
@@ -16,12 +14,6 @@ app.command(no_args_is_help=True)(gen)
 app.command(no_args_is_help=True)(declare_project)
 app.command(no_args_is_help=True)(deploy_project)
 app.command(no_args_is_help=True)(verify_onchain)
-
-
-@app.command()
-def hi(name: Annotated[str, typer.Argument()]):
-    """Say hello to someone"""
-    print(f"Hello {name}")
 
 
 if __name__ == "__main__":
