db4825c6d7e7403024a3470ceec88536ab0ec705
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2024-09-05 19:54:41 -0300

    Adds preliminary configuration of the integration-test-suite folder

---
+++ tools/npm/integration-test-suite/packages/web-js-esm-webpack/src/index.js
@@ -0,0 +1,10 @@
+import * as garaga_rs from 'garaga_rs';
+
+async function main() {
+  await garaga_rs.init();
+  const result = garaga_rs.msm_calldata_builder([1, 2], [10], 0);
+  console.log('Output of msm_calldata_builder:', result);
+}
+
+main()
+  .catch(console.error);
