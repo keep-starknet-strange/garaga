27e4b6467d5dfe4c145c87c72cc5da2a6b1a1e1f
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2024-09-09 12:48:54 -0300

    Makes adjustments to web based tests to capture only relevant output

--- tools/npm/integration-test-suite/packages/web-js-esm-react/test.cjs
+++ tools/npm/integration-test-suite/packages/web-js-esm-react/test.cjs
@@ -6,20 +6,25 @@ function loadFileContent(name) {
   return fs.readFileSync(path.join(__dirname, name), 'utf8');
 }
 
+function writeFileContent(name, content) {
+  return fs.writeFileSync(path.join(__dirname, name), content, 'utf8');
+}
+
 async function loadWebContent(url) {
   const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
   const page = await browser.newPage();
   await page.goto(url);
-  await page.waitForSelector('#dynamic-content', { visible: true });
-  const content = await page.content();
+  await page.waitForSelector('body pre', { visible: true });
+  const content = await page.evaluate(() => document.querySelector('body pre').innerHTML);
   await browser.close();
   return content;
 }
 
 async function test() {
-  const content1 = loadFileContent('index.html');
+  const content1 = loadFileContent('output.txt');
   const content2 = await loadWebContent('http://localhost:8080');
   if (content1 !== content2) {
+    writeFileContent('output.txt', content2);
     throw new Error('Content mistmatch');
   }
 }
