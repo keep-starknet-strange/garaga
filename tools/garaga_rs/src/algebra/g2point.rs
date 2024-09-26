5e1b546771e55e7f4b02fd21164d34965bd68df5
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2024-09-26 15:35:53 -0300

    Rewrites G2Point::new

--- tools/garaga_rs/src/algebra/g2point.rs
+++ tools/garaga_rs/src/algebra/g2point.rs
@@ -1,4 +1,4 @@
-use crate::algebra::extf_mul::to_e2;
+use crate::algebra::extf_mul::{from_e2, to_e2};
 use lambdaworks_math::field::element::FieldElement;
 use lambdaworks_math::field::traits::{IsField, IsPrimeField, IsSubFieldOf};
 
@@ -17,17 +17,19 @@ where
     F: IsPrimeField + IsSubFieldOf<E2>,
     E2: IsField<BaseType = [FieldElement<F>; 2]>,
 {
-    pub fn new(x0: [FieldElement<F>; 2], y0: [FieldElement<F>; 2]) -> Result<Self, String> {
-        let x = to_e2(x0.clone());
-        let y = to_e2(y0.clone());
+    pub fn new(x: [FieldElement<F>; 2], y: [FieldElement<F>; 2]) -> Result<Self, String> {
+        let x = to_e2(x);
+        let y = to_e2(y);
         let point = Self { x, y };
         if !point.is_infinity() && !point.is_on_curve() {
+            let x = from_e2(point.x);
+            let y = from_e2(point.y);
             return Err(format!(
                 "Point (({:?}, {:?}), ({:?}, {:?})) is not on the curve",
-                x0[0].representative().to_string(),
-                x0[1].representative().to_string(),
-                y0[0].representative().to_string(),
-                y0[1].representative().to_string(),
+                x[0].representative().to_string(),
+                x[1].representative().to_string(),
+                y[0].representative().to_string(),
+                y[1].representative().to_string(),
             ));
         }
         Ok(point)
