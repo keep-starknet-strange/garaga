73737fee24572261488d4d82902a070e133d170d
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2024-10-03 12:09:03 -0300

    Fixes default option handling in garaga_ts

--- tools/npm/garaga_ts/src/node/api.ts
+++ tools/npm/garaga_ts/src/node/api.ts
@@ -32,10 +32,10 @@ export type MsmCalldataBuilderOptons = Partial<{
 
 export function msmCalldataBuilder(points: G1Point[], scalars: bigint[], curveId: CurveId, options: MsmCalldataBuilderOptons = {}): bigint[] {
   const values = flatten(points);
-  const includeDigitsDecomposition = options.includeDigitsDecomposition || true;
-  const includePointsAndScalars = options.includePointsAndScalars || true;
-  const serializeAsPureFelt252Array = options.serializeAsPureFelt252Array || false;
-  const risc0Mode = options.risc0Mode || false;
+  const includeDigitsDecomposition = options.includeDigitsDecomposition ?? true;
+  const includePointsAndScalars = options.includePointsAndScalars ?? true;
+  const serializeAsPureFelt252Array = options.serializeAsPureFelt252Array ?? false;
+  const risc0Mode = options.risc0Mode ?? false;
   return msm_calldata_builder(values, scalars, curveId, includeDigitsDecomposition, includePointsAndScalars, serializeAsPureFelt252Array, risc0Mode);
 }
 
