47efa2272528c966535c75be7ac19042f8161ab0
Luiz Vasconcelos Júnior <64055364+luiz-lvj@users.noreply.github.com>
2024-10-03 05:26:47 -0300

    Ed25519 Twisted Edwards to Weirstrass bijection in wasm pkg (#218)
    Co-authored-by: feltroid Prime <96737978+feltroidprime@users.noreply.github.com>

--- tools/npm/garaga_ts/src/node/api.ts
+++ tools/npm/garaga_ts/src/node/api.ts
@@ -1,6 +1,6 @@
 // This files provides a ts-like interface for garaga_rs
 
-import { msm_calldata_builder } from '../wasm/pkg/garaga_rs';
+import { msm_calldata_builder, to_twistededwards, to_weirstrass  } from '../wasm/pkg/garaga_rs';
 
 export enum CurveId {
   BN254 = 0,
@@ -26,4 +26,24 @@ export function msmCalldataBuilder(points: G1Point[], scalars: bigint[], curveId
   const serializeAsPureFelt252Array = options.serializeAsPureFelt252Array ?? false;
   const risc0Mode = options.risc0Mode ?? false;
   return msm_calldata_builder(values, scalars, curveId, includeDigitsDecomposition, includePointsAndScalars, serializeAsPureFelt252Array, risc0Mode);
+}
+
+export function toWeirstrass( x_twisted: bigint, y_twisted: bigint): [bigint, bigint] {
+  const result = to_weirstrass(x_twisted, y_twisted);
+
+  if(result.length !== 2) {
+    throw new Error('Invalid result length');
+  }
+
+  return [result[0], result[1]];
+}
+
+export function toTwistedEdwards( x_weierstrass: bigint, y_weierstrass: bigint): [bigint, bigint] {
+  const result = to_twistededwards(x_weierstrass, y_weierstrass);
+
+  if(result.length !== 2) {
+    throw new Error('Invalid result length');
+  }
+
+  return [result[0], result[1]];
 }
