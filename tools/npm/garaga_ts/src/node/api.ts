4fba244677902ea19fa985567081aadd117cb49f
Luiz <lvj253@gmail.com>
2024-10-29 11:51:44 -0300

    adding get_groth16_calldata to ts api

--- tools/npm/garaga_ts/src/node/api.ts
+++ tools/npm/garaga_ts/src/node/api.ts
@@ -1,6 +1,7 @@
 // This files provides a ts-like interface for garaga_rs
 
-import { msm_calldata_builder, mpc_calldata_builder, to_twistededwards, to_weirstrass } from '../wasm/pkg/garaga_rs';
+import { msm_calldata_builder, mpc_calldata_builder, to_twistededwards, to_weirstrass, get_groth16_calldata } from '../wasm/pkg/garaga_rs';
+import { Groth16Proof, Groth16VerifyingKey } from './starknet/groth16ContractGenerator/parsingUtils';
 
 export enum CurveId {
   BN254 = 0,
@@ -63,4 +64,10 @@ export function toTwistedEdwards(x_weierstrass: bigint, y_weierstrass: bigint): 
   }
 
   return [result[0], result[1]];
+}
+
+export function getGroth16CallData(proof: Groth16Proof, verifyingKey: Groth16VerifyingKey, curveId: CurveId){
+
+  return get_groth16_calldata(proof, verifyingKey, curveId);
+
 }
