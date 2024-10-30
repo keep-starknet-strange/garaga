cb6efdc33450db9698a7fedbb1a4dc3dca760d7c
feltroidprime <feltroidprime@protonmail.com>
2024-10-30 11:33:09 +0700

    use local bb

--- tools/noir/run.sh
+++ tools/noir/run.sh
@@ -19,31 +19,38 @@ reset
 # (git version hash: 801c71880ecf8386a26737a5d8bb5b4cb164b2ab, is dirty: false)
 # bb version : 0.58.0
 
+BB_PATH="/home/felt/PycharmProjects/aztec2/aztec-packages/barretenberg/cpp/build/bin/bb"
+
+
 echo "nargo version : $(nargo --version)" # 0.36.0
-echo "bb version : $(bb --version)" # 0.58.0
+echo "bb version : $($BB_PATH --version)" # 0.58.0
 
 
 run_noir_proof_basic() {
     cd hello
     local suffix="_basic"
-    bb prove -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
-    bb write_vk -b target/hello.json -o target/vk${suffix}.bin
-    bb verify -p target/proof${suffix}.bin -k target/vk${suffix}.bin
-    bb contract -k target/vk${suffix}.bin -o target/contract${suffix}.sol
+    $BB_PATH prove -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
+    $BB_PATH write_vk -b target/hello.json -o target/vk${suffix}.bin
+    if $BB_PATH verify -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    $BB_PATH contract -k target/vk${suffix}.bin -o target/contract${suffix}.sol
     cd ../
 }
 
 run_noir_proof_ultra() {
     cd hello
     local suffix="_ultra"
-    bb prove_ultra_honk -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
-    bb write_vk_ultra_honk -b target/hello.json -o target/vk${suffix}.bin
-    if bb verify_ultra_honk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
-        echo "ok"
+    $BB_PATH prove_ultra_honk -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
+    $BB_PATH write_vk_ultra_honk -b target/hello.json -o target/vk${suffix}.bin
+    if $BB_PATH verify_ultra_honk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
     else
-        echo "Verification failed"
+        echo "Verification failed $suffix"
     fi
-    bb contract_ultra_honk -k target/vk${suffix}.bin -o target/contract${suffix}.sol
+    $BB_PATH contract_ultra_honk -k target/vk${suffix}.bin -o target/contract${suffix}.sol
     cd ../
 }
 
@@ -51,14 +58,14 @@ run_noir_proof_ultra_keccak() {
     cd hello
     local suffix="_ultra_keccak"
 
-    bb prove_ultra_keccak_honk -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
-    bb write_vk_ultra_keccak_honk -b target/hello.json -o target/vk${suffix}.bin
-    if bb verify_ultra_keccak_honk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
-        echo "ok"
+    $BB_PATH prove_ultra_keccak_honk -b target/hello.json -w target/witness.gz -o target/proof${suffix}.bin
+    $BB_PATH write_vk_ultra_keccak_honk -b target/hello.json -o target/vk${suffix}.bin
+    if $BB_PATH verify_ultra_keccak_honk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
     else
-        echo "Verification failed"
+        echo "Verification failed $suffix"
     fi
-    bb contract_ultra_honk -k target/vk${suffix}.bin -o target/contract${suffix}.sol # contract_ultra_keccak_honk does not exist
+    $BB_PATH contract_ultra_honk -k target/vk${suffix}.bin -o target/contract${suffix}.sol # contract_ultra_keccak_honk does not exist
     cd ../
 }
 
