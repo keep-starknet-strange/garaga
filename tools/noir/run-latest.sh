306cb9911220f4dae42af4a743b767d03d13df52
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2025-03-17 12:28:53 -0300

    Adds a version of tools/noir/run.sh supporting the latest version of bb

---
+++ tools/noir/run-latest.sh
@@ -0,0 +1,182 @@
+reset() {
+    rm -rf hello/
+    nargo new hello
+    cd hello
+    nargo check
+    cat << EOF > Prover.toml
+x = "1"
+y = "2"
+EOF
+    nargo execute witness
+    cd ../
+}
+
+reset
+
+
+BB_PATH="$HOME/Projects/Executed/onlydust/garaga/tmp/aztec-packages/barretenberg/cpp/build/bin/bb"
+
+
+echo "nargo version : $(nargo --version)" # 1.0.0-beta.2
+echo "bb version : $($BB_PATH --version)" # 0.74.0
+
+
+run_noir_proof_basic() {
+    cd hello
+    local suffix="_basic"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    cd ../
+}
+
+run_noir_proof_ultra() {
+    cd hello
+    local suffix="_ultra"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove -s ultra_honk --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -s ultra_honk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    cd ../
+}
+
+run_noir_proof_ultra_keccak() {
+    cd hello
+    local suffix="_ultra_keccak"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove -s ultra_honk --oracle_hash keccak --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -s ultra_honk --oracle_hash keccak -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    $BB_PATH write_solidity_verifier -s ultra_honk -k target/vk${suffix}.bin -o target/contract${suffix}.sol
+    cd ../
+}
+
+run_noir_proof_ultra_starknet() {
+    cd hello
+    local suffix="_ultra_starknet"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove -s ultra_honk --oracle_hash starknet --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -s ultra_honk --oracle_hash starknet -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    cd ../
+}
+
+run_noir_proof_ultra_keccak_zk() {
+    cd hello
+    local suffix="_ultra_keccak_zk"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove -s ultra_honk --oracle_hash keccak --zk --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -s ultra_honk --oracle_hash keccak --zk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    $BB_PATH write_solidity_verifier -s ultra_honk --zk -k target/proof${suffix}.bin -o target/contract${suffix}.sol
+    cd ../
+}
+
+run_noir_proof_ultra_starknet_zk() {
+    cd hello
+    local suffix="_ultra_starknet_zk"
+
+    mkdir -p target/proof${suffix}/
+    $BB_PATH prove -s ultra_honk --oracle_hash starknet --zk --write_vk --output_format bytes_and_fields -b target/hello.json -w target/witness.gz -o target/proof${suffix}/
+    mv -f target/proof${suffix}/proof target/proof${suffix}.bin
+    mv -f target/proof${suffix}/proof_fields.json target/proof${suffix}.json
+    mv -f target/proof${suffix}/vk target/vk${suffix}.bin
+    mv -f target/proof${suffix}/vk_fields.json target/vk${suffix}.json
+    rmdir target/proof${suffix}/
+
+    if $BB_PATH verify -s ultra_honk --oracle_hash starknet --zk -p target/proof${suffix}.bin -k target/vk${suffix}.bin; then
+        echo "ok $suffix"
+    else
+        echo "Verification failed $suffix"
+    fi
+    cd ../
+}
+
+
+
+echo $'\n basic'
+# reset
+run_noir_proof_basic
+
+echo $'\n ultra honk'
+# reset
+run_noir_proof_ultra
+
+echo $'\n ultra keccak honk'
+# reset
+run_noir_proof_ultra_keccak
+
+echo $'\n ultra starknet honk'
+# reset
+run_noir_proof_ultra_starknet
+
+echo $'\n ultra keccak zk honk'
+# reset
+run_noir_proof_ultra_keccak_zk
+
+echo $'\n ultra starknet zk honk'
+# reset
+run_noir_proof_ultra_starknet_zk
+
+
+echo $'\n'
+# Print sha256 hash of of .sol files :
+for file in hello/target/*.sol; do
+    echo $(md5sum $file)
+done
+
+# Print sha256 hash of of .bin files :
+for file in hello/target/*.bin; do
+    echo $(md5sum $file)
+done
