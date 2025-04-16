cfdd58bc085ac529e24c748c688ce086945c8dde
Rodrigo Ferreira <rodrigo.ferreira@aya.yale.edu>
2025-04-16 17:24:12 -0300

    Attempts to fix garaga_ts CI

--- tools/npm/garaga_ts/tests/starknet/honkCalldata.test.ts
+++ tools/npm/garaga_ts/tests/starknet/honkCalldata.test.ts
@@ -7,23 +7,26 @@ const PATH = '../../../hydra/garaga/starknet/honk_contract_generator/examples';
 describe('Honk Getting calldata', () => {
 
     const proofAndVkWithFlavors = [
-        [`${PATH}/proof_ultra_keccak.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.KECCAK],
-        [`${PATH}/proof_ultra_starknet.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.STARKNET],
+        [`${PATH}/proof_ultra_keccak.bin`, `${PATH}/public_inputs_ultra_keccak_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.KECCAK],
+        [`${PATH}/proof_ultra_starknet.bin`, `${PATH}/public_inputs_ultra_keccak_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.STARKNET],
     ]
 
-    test.each(proofAndVkWithFlavors)("should  get honk calldata from proof %s, vk %s and pub inputs %s", async (proofPath, vkPath, flavor) => {
+    test.each(proofAndVkWithFlavors)("should  get honk calldata from proof %s, public inputs %s, vk %s and pub inputs %s", async (proofPath, publicInputsPath, vkPath, flavor) => {
 
         await garaga.init();
 
         const vkBytes = new Uint8Array(fs.readFileSync(vkPath as string));
 
+        const publicInputsBytes = new Uint8Array(fs.readFileSync(publicInputsPath as string));
+
         const proofBytes = new Uint8Array(fs.readFileSync(proofPath as string));
 
         console.log("proof", proofBytes);
+        console.log("publicInputs", publicInputsBytes);
         console.log("vk", vkBytes);
         console.log("flavor", flavor);
 
-        const honkCalldata = garaga.getHonkCallData(proofBytes, vkBytes, flavor as HonkFlavor);
+        const honkCalldata = garaga.getHonkCallData(proofBytes, publicInputsBytes, vkBytes, flavor as HonkFlavor);
 
         console.log("honkCalldata", honkCalldata);
 
--- tools/npm/garaga_ts/tests/starknet/zkHonkCalldata.test.ts
+++ tools/npm/garaga_ts/tests/starknet/zkHonkCalldata.test.ts
@@ -7,23 +7,26 @@ const PATH = '../../../hydra/garaga/starknet/honk_contract_generator/examples';
 describe('ZK Honk Getting calldata', () => {
 
   const proofAndVkWithFlavors = [
-    [`${PATH}/proof_ultra_keccak_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.KECCAK],
-    [`${PATH}/proof_ultra_starknet_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.STARKNET],
+    [`${PATH}/proof_ultra_keccak_zk.bin`, `${PATH}/public_inputs_ultra_keccak_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.KECCAK],
+    [`${PATH}/proof_ultra_starknet_zk.bin`, `${PATH}/public_inputs_ultra_keccak_zk.bin`, `${PATH}/vk_ultra_keccak.bin`, HonkFlavor.STARKNET],
   ]
 
-  test.each(proofAndVkWithFlavors)("should  get zk honk calldata from proof %s, vk %s and pub inputs %s", async (proofPath, vkPath, flavor) => {
+  test.each(proofAndVkWithFlavors)("should  get zk honk calldata from proof %s, public inputs %s, vk %s and pub inputs %s", async (proofPath, publicInputsPath, vkPath, flavor) => {
 
     await garaga.init();
 
     const vkBytes = new Uint8Array(fs.readFileSync(vkPath as string));
 
+    const publicInputsBytes = new Uint8Array(fs.readFileSync(publicInputsPath as string));
+
     const proofBytes = new Uint8Array(fs.readFileSync(proofPath as string));
 
     console.log("proof", proofBytes);
+    console.log("publicInputs", publicInputsBytes);
     console.log("vk", vkBytes);
     console.log("flavor", flavor);
 
-    const honkCalldata = garaga.getZKHonkCallData(proofBytes, vkBytes, flavor as HonkFlavor);
+    const honkCalldata = garaga.getZKHonkCallData(proofBytes, publicInputsBytes, vkBytes, flavor as HonkFlavor);
 
     console.log("honkCalldata", honkCalldata);
 
