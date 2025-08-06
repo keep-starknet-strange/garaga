abdc4bd8dceb09bf6035b4c9dc8cc2f2a27db908
GitHub Docs Sync <docs-sync@garaga.dev>
2025-08-06 12:31:00 +0000

    🔄 Sync all files from main @ d0b6b2046717ef3ed7d77998d0c3b0dd5107b71a

--- tools/npm/garaga_ts/src/constants.ts
+++ tools/npm/garaga_ts/src/constants.ts
@@ -1,23 +1,31 @@
 /**
- * Auto-generated constants file. Do not edit manually.
- * Generated from constants.json by tools/make/generate_constants.py
+ * Auto-generated constants file from constants.json. Do not edit manually.
  */
 
 // RISC0 Constants
-// https://github.com/risc0/risc0-ethereum/blob/main/contracts/src/groth16/ControlID.sol
-// release 2.0
-export const RISC0_CONTROL_ROOT = BigInt("0x539032186827B06719244873B17B2D4C122E2D02CFB1994FE958B2523B844576");
-export const RISC0_BN254_CONTROL_ID = BigInt("0x04446E66D300EB7FB45C9726BB53C793DDA407A62E9601618BB43C5C14657AC0");
+// https://github.com/risc0/risc0-ethereum/blob/v2.2.2/contracts/src/groth16/ControlID.sol
+// release v2.2.2
+export const RISC0_CONTROL_ROOT = BigInt(
+  "0xCE52BF56033842021AF3CF6DB8A50D1B7535C125A34F1A22C6FDCF002C5A1529",
+);
+export const RISC0_BN254_CONTROL_ID = BigInt(
+  "0x04446E66D300EB7FB45C9726BB53C793DDA407A62E9601618BB43C5C14657AC0",
+);
 
 // SP1 Constants
-// https://github.com/succinctlabs/sp1-contracts/blob/main/contracts/src/v4.0.0-rc.3/SP1VerifierGroth16.sol
-export const SP1_VERIFIER_VERSION: string = "v4.0.0-rc.3";
-export const SP1_VERIFIER_HASH: string = "0x11b6a09d63d255ad425ee3a7f6211d5ec63fbde9805b40551c3136275b6f4eb4";
+// https://github.com/succinctlabs/sp1-contracts/blob/main/contracts/src/v5.0.0/SP1VerifierGroth16.sol
+export const SP1_VERIFIER_VERSION = "v5.0.0";
+export const SP1_VERIFIER_HASH =
+  "0xa4594c59bbc142f3b81c3ecb7f50a7c34bc9af7c4c444b5d48b795427e285913";
 
 // Additional RISC0 constants for internal use
-export const RISC0_SYSTEM_STATE_ZERO_DIGEST = Uint8Array.from(Buffer.from(
+export const RISC0_SYSTEM_STATE_ZERO_DIGEST = Uint8Array.from(
+  Buffer.from(
     "A3ACC27117418996340B84E5A90F3EF4C49D22C79E44AAD822EC9C313E1EB8E2",
-    "hex"
-));
-export const RISC0_TAG_DIGEST = "0xcb1fefcd1f2d9a64975cbbbf6e161e2914434b0cbb9960b84df5d717e86b48af";
-export const RISC0_OUTPUT_TAG = "0x77eafeb366a78b47747de0d7bb176284085ff5564887009a5be63da32d3559d4";
+    "hex",
+  ),
+);
+export const RISC0_TAG_DIGEST =
+  "0xcb1fefcd1f2d9a64975cbbbf6e161e2914434b0cbb9960b84df5d717e86b48af";
+export const RISC0_OUTPUT_TAG =
+  "0x77eafeb366a78b47747de0d7bb176284085ff5564887009a5be63da32d3559d4";
