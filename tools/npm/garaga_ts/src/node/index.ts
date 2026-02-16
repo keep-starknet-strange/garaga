b36e0c2510fc9e84f6dfd2c079dae66e46029202
Godspower Eze <godspower.eth@gmail.com>
2026-02-16 23:35:52 +0300

    fix: update

--- tools/npm/garaga_ts/src/node/index.ts
+++ tools/npm/garaga_ts/src/node/index.ts
@@ -1,5 +1,5 @@
-export * from './api';
-export * from '../wasm/pkg/garaga_rs';
+export * from './api'; // exports "ts" interface
+export * from '../wasm/pkg/garaga_rs'; // export "raw" interface
 export * from './starknet/groth16ContractGenerator/parsingUtils';
 
 export { CurveId } from './definitions';
